33be018d683deec9b106f275e706325d3c48f15c
Jeffrey Alvarado <jefalva10296@gmail.com>
2017-10-11 21:56:27 -0600

    initial commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Calculator
+{
+    private float accumulator;
+
+    public Calculator()
+	{
+        accumulator = 0;
+	}
+
+    public float add(float operand1, float operand2)
+    {
+        return (operand1 + operand2);
+    }
+
+    public float substract(float operand1, float operand2)
+    {
+        return (operand1 - operand2);
+    }
+
+    public float multiply(float operand1, float operand2)
+    {
+        return (operand1 * operand2);
+    }
+
+    public float divide(float operand1, float operand2)
+    {
+        return (operand1 / operand2);
+    }
+
+    public void save(float number)
+    {
+        accumulator = number;
+    }
+
+    public float read()
+    {
+        return accumulator;
+    }
+
+    public void erase()
+    {
+        accumulator = 0;
+    }
+}
---
+++ Calculator/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculatorTest
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.save(calculator.multiply(100, 10));
+            Console.WriteLine(calculator.multiply(calculator.read(), 30));
+            Console.ReadKey();
+        }
+    }
+}
