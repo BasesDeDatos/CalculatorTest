c3e6d2d5ea52bff8b23c7759fb98ab14d6969c84
Jeffrey Alvarado <jefalva10296@gmail.com>
2017-10-11 22:31:31 -0600

    Initial commit

--- Calculator/CalculatorTest.cs
+++ Calculator/CalculatorTest.cs
@@ -1,15 +1,45 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace CalculatorTest
 {
+    [TestFixture]
     class CalculatorTest
     {
-        static void Main(string[] args)
+        [TestCase]
+        public void addCase()
         {
             Calculator calculator = new Calculator();
-            calculator.save(calculator.multiply(100, 10));
-            Console.WriteLine(calculator.multiply(calculator.read(), 30));
-            Console.ReadKey();
+            Assert.AreEqual(12, (calculator.add(4, 8)));
         }
+
+        [TestCase]
+        public void substractCase()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(24, (calculator.add(50, 26)));
+        }
+
+        [TestCase]
+        public void multiplyCase()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(48, (calculator.add(2, 24)));
+        }
+
+        [TestCase]
+        public void divideCase()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(3, (calculator.add(9, 3)));
+        }
+
+        [TestCase]
+        public void readCase()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(0, (calculator.read()));
+        }
+        
     }
 }
